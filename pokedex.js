13a1e737ecf75d977f3e66d4c9b3ffa779c5e6ed
MMAndresC <mandrescasabon@hotmail.com>
2022-05-27 00:23:35 +0200

    Añadido barra de busqueda de pokemon por nombre e id

--- pokedex.js
+++ pokedex.js
@@ -1,4 +1,29 @@
 const MAPPED_POKEMON = [];
+const TYPES = [
+    "All",
+    "Normal",
+    "Fighting",
+    "Flying",
+    "Poison",
+    "Ground",
+    "Rock",
+    "Bug",
+    "Ghost",
+    "Steel",
+    "Fire",
+    "Water",
+    "Grass",
+    "Electric",
+    "Psychic",
+    "Ice",
+    "Dragon",
+    "Dark",
+    "Fairy",
+    "Unknown",
+    "Shadow",
+  ];
+  
+
 
 const getDataPokemonFromApi = (url) => {
     return fetch (url)
@@ -6,8 +31,6 @@ const getDataPokemonFromApi = (url) => {
         .then (DataApiPokemon => DataApiPokemon)
         .catch (error => console.log('No se ha podido recuperar los datos de la API', url, error));
 }
-
-
 
 const extractData = (data) =>{
     const aux = {};
@@ -21,31 +44,57 @@ const extractData = (data) =>{
     MAPPED_POKEMON.push(aux);
 } 
 
-const filterPokemonByType = (nameType) =>{
-    const filteredPokemonList = pokemonList.filter(pokemon => pokemon.types.include(nameType));
+const filterPokemonBy = (search,listPokemons) =>{
+    search = search.toLowerCase();
+    let filteredPokemonList = listPokemons.filter(pokemon => pokemon.name.includes(search));  
+    //filteredPokemonList = listPokemons.filter(pokemon => pokemon.id === search);
+    if(filteredPokemonList.length ===  0){
+        filteredPokemonList = listPokemons.filter(pokemon => pokemon.id === Number(search));
+    }
     return filteredPokemonList;
 }
 
-const drawPokedex = () => { 
+const drawPokedex = (listPokemons) => { 
     const olPokemon$$ = document.querySelector('ol');
     olPokemon$$.innerHTML = '';
-    MAPPED_POKEMON.forEach(pokemon => {
-        const li$$ = document.createElement('li');
+    for(pokemon of listPokemons) {
+        const li$$ = document.createElement('li'); //MIRAR COMO SE PONE LA PRIMERA LETRA DE UNA STRING EN MAYUSCULAS MEJOR QUE PONERLO TODO
         li$$.innerHTML = `
-            <h2>${pokemon.name}</h2>
-            <div>
-                <img src=${pokemon.img} alt=${pokemon.name} />
+            <h2>${pokemon.name.toUpperCase()}</h2> 
+            <div class="pokedex-card--image-container">
+                <img src=${pokemon.img} alt=${pokemon.name} class="pokedex-card--image"/>
             </div>
         `;
         li$$.className = 'pokedex-card';
         olPokemon$$.appendChild(li$$);
+    }
+}
+
+const drawOptionTypes = () => {
+    const section = document.querySelector('.option-types');
+    for (const type of TYPES){
+        section.innerHTML += `
+        <input type="checkbox" name=types class="option-types-checkbox"/>${type}`
+    }
+}
+
+const inputSearch = () => {
+    const searchInput = document.querySelector('[type=search]');
+    searchInput.addEventListener('input',event => {
+        drawPokedex(filterPokemonBy(searchInput.value,MAPPED_POKEMON));
     })
+
+}
+
+const drawSectionFilters = () => {
+    drawOptionTypes();
+    inputSearch();
 }
 
 
 
 const initPokedex = async() => {
-    const urlApi = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20';
+    const urlApi = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20'; //ACORDARME DE CAMBIAR EL Nº DE POKEMON A 151
    
 
     const listAllPokemons = await getDataPokemonFromApi(urlApi);
@@ -54,8 +103,8 @@ const initPokedex = async() => {
         extractData(dataTotal);
     }
   
-    drawPokedex();
-    
+    drawPokedex(MAPPED_POKEMON);
+    drawSectionFilters();
     
 }
 
