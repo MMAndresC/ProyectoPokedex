4d38a6525634b6c40583dad7dc0e746345602d32
MMAndresC <mandrescasabon@hotmail.com>
2022-05-26 18:20:00 +0200

    Funciones para recuperar datos de la API

---
+++ pokedex.js
@@ -0,0 +1,61 @@
+const MAPPED_POKEMON = [];
+
+const getDataPokemonFromApi = (url) => {
+    return fetch (url)
+        .then (response => response.json())
+        .then (DataApiPokemon => DataApiPokemon)
+        .catch (error => console.log('No se ha podido recuperar los datos de la API', url, error));
+}
+
+
+
+const extractData = (data) =>{
+    const aux = {};
+    aux.name = data.name;
+    aux.id = data.id;
+    aux.sprite = data.sprites.front_default;
+    aux.img = data.sprites.other.dream_world.front_default;
+    aux.types =[];
+    aux.types = data.types.map(type => type.type.name );
+    //EXTRAER ATAQUES
+    MAPPED_POKEMON.push(aux);
+} 
+
+const filterPokemonByType = (nameType) =>{
+    const filteredPokemonList = pokemonList.filter(pokemon => pokemon.types.include(nameType));
+    return filteredPokemonList;
+}
+
+const drawPokedex = () => { 
+    const olPokemon$$ = document.querySelector('ol');
+    olPokemon$$.innerHTML = '';
+    MAPPED_POKEMON.forEach(pokemon => {
+        const li$$ = document.createElement('li');
+        li$$.innerHTML = `
+            <h2>${pokemon.name}</h2>
+            <img src=${pokemon.img} />
+        `;
+        olPokemon$$.appendChild(li$$);
+    })
+}
+
+
+
+const initPokedex = async() => {
+    const urlApi = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100';
+   
+
+    const listAllPokemons = await getDataPokemonFromApi(urlApi);
+    for (const result of listAllPokemons.results) {
+        const dataTotal = await getDataPokemonFromApi(result.url);
+        extractData(dataTotal);
+    }
+  
+    drawPokedex();
+    
+    
+}
+
+
+
+initPokedex();
